e0e89c6d60139b4e06e9666f607f8aacef4d428a
Bryan M <janbryanmartirez@gmail.com>
2018-11-21 17:45:18 +0800

    express-graphql

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-var { graphql, buildSchema } = require('graphql');
+const express = require ('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
+const crypto = require ('crypto')
 
 const db = {
     users: [
-        { id: '1', email: 'bryan@gmail.com', name: 'Bry'},
-        { id: '2', email: 'awyis@gmail.com', name: 'Awyis'},
+        { id: '1', email: 'bryan@gmail.com', name: 'Bry', avatarUrl: 'Awyssss' },
+        { id: '2', email: 'awyis@gmail.com', name: 'Awyis', avatarUrl: 'Aaweaer'}, 
     ]
 }
 
 const schema = buildSchema(`
   type Query {
     users: [User!]!
+  }
+
+  type Mutation {
+      addUser(email: String!, name: String): User
   }
 
   type User {
@@ -20,22 +27,43 @@ const schema = buildSchema(`
   }
 `);
 
-var rootValue = { 
-    users: () => db.users
+const rootValue = { 
+    users: () => db.users,
+    addUser: ({email, name}) => {
+        const user = {
+            id: crypto.randomBytes(10).toString('hex'),
+            email,
+            name
+        }
+        db.users.push(user)
+        console.log(db.users)
+        return user
+
+    }
 };
 
-graphql(schema, 
-    `
-        { 
-            users {
-                id
-                email
-            }     
-        }
-    `,
-    rootValue    
-).then((response) => {
-  console.dir(response, { depth: null });
-}).catch(
-    console.error
-)
+// graphql(schema, 
+//     `
+//         { 
+//             users {
+//                 id
+//                 email
+//             }     
+//         }
+//     `,
+//     rootValue    
+// ).then((response) => {
+//   console.dir(response, { depth: null });
+// }).catch(
+//     console.error
+// )
+
+const app = express()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue,
+    graphiql: true
+}))
+
+app.listen(3000, () => console.log('Listening on port 3000'))
